be964d3c7b195476a70d0eba33916414295e79f4
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-16 17:46:24 -0800

    Print size of datatypes on 32bit & 64bit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,10 +6,10 @@
  */
 int main(void)
 {
-    printf("size of a char: %ld byte(s)\n", sizeof(char));
-    printf("size of an int: %ld byte(s)\n", sizeof(int));
-    printf("size of a long int: %ld byte(s)\n", sizeof(long int));
-    printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
-    printf("size of a float: %ld byte(s)\n", sizeof(float));
-    return (0);
+printf("size of a char: %ld byte(s)\n", sizeof(char));
+printf("size of an int: %ld byte(s)\n", sizeof(int));
+printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("size of a float: %ld byte(s)\n", sizeof(float));
+return (0);
 }
