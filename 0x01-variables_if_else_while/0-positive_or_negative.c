2c1aecee037e0c06557bb4d10c7f36e001349909
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-17 10:33:09 -0800

    If_Else Pos/Neg Output

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,18 +8,16 @@
  */
 int main(void)
 {
-  int n;
-
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-
-    if (n > 0)
-      {
-	printf("%d is positive", n);
-      }
-    else
-      {
-	printf("%d is negative", n);
-      }
-  reutrn (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive", n);
+}
+else
+{
+printf("%d is negative", n);
+}
+return (0);
 }
