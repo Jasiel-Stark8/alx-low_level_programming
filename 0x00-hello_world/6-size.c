e91ee0414bd02268544ff645047af6621fb34bc9
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-16 17:14:06 -0800

    Print size of datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**                                                                                     
+ * main - Entry point                                                                   
+ *                                                                                      
+ * Return: Always 0 (Success)                                                           
+ */
+int main(void)
+{
+printf("size of a char: %c bytes(s)" (char));
+printf("size of an int: %d bytes(s)" (int));
+printf("size of a long int: %ld bytes(s)" (long int));
+printf("size of a long long int: %lld bytes(s)" (long long int));
+printf("size of a float: %f bytes(s)" (float));
+return (0);
+}
