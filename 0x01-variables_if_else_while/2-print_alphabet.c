e5ed83830dd95ef6cc7d1d159b658a2519824079
Jason Kvïst <94616615+Jasiel-Stark8@users.noreply.github.com>
2023-02-18 21:36:50 +0000

    For Loops Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-  char alph[26];
-      for (int letter = 0; letter < 26; letter++)
-        {
-          alph[letter] = 'a' + letter;
-          putchar(alph[letter]);
-        }
-    putchar('\n');
-    return (0);
+  int letter;
+  for (letter = 0; letter < 26; letter++)
+    {
+      alph[letter] = 'a' + letter;
+      putchar('a' + letter);
+    }
+  putchar('\n');
+  return (0);
 }
