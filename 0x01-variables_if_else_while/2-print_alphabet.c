936f58691413a3a347f5ee52f21b767e193d83b0
Jason Kvïst <94616615+Jasiel-Stark8@users.noreply.github.com>
2023-02-18 21:45:56 +0000

    alphabet for_loop

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-/**                                                                     
- * main - Entry point                                                   
+/**
+ * main - Entry point
  *                                                                      
- * Return: Always 0 (Success)                                           
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
