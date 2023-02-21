3509bcca25275889f9c69b67a6e6f55becaf8b5d
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-21 13:25:26 -0800

    void print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  for (int letter = 'a'; lettter < 27; letter++);
+    _putchar(letter);
+    
+  _putchar('\n');
+}
