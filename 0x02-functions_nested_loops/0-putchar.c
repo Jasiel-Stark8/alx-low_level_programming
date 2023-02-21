cd100f6585d7c6de060b8a93a7ef783b34b26c9f
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-21 10:52:38 -0800

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return(0);
+}
