fa35f74c34a0e9b6ced685d1bfad3ed14bba7e66
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-21 15:19:34 -0800

    Function prototype header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef main_h
 #define main_h
 
-// Declarations for functions
+/* Declarations for functions */
 
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
