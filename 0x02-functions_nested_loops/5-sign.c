9369b1ea2833d9afa298e9cfb47fe8a8749ec7f2
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-21 19:34:03 -0800

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
-/** 
+/**
  * print_sign - Entr point
- * @n: carrier variable  
+ * @n: carrier variable 
  * Description: prints the sign of a number
  * Return: 1 if n>0, 0 if n == 0, -1 if n<0
  */
@@ -18,7 +18,7 @@ _putchar('0');
 return (0);
 }
 else
-{	
+{
 _putchar('+');
 return (1);
 }
