a2f96a05382a2b3c694b38f05c6a46b1d38e04f2
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-21 16:39:31 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,11 @@
  */
 void print_alphabet(void)
 {
-  char letter = 'a';
-  while (letter <= 'z')
-    {
-      _putchar(letter);
-      letter++;
-    }
-  _putchar('\n');
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
 }
