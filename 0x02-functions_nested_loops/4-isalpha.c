be610eb8adf5c5f7ac6ce3644c989092d832c370
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-21 18:59:54 -0800

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+/**
+ * _islower - checks for lowercase character
+ * @c: character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
