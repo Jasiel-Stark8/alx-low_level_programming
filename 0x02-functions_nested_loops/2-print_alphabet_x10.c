82f1c204ee90e759b39819d9ebabe5d892be379c
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-21 17:25:08 -0800

    print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,27 +5,18 @@
  *
  * Return: Always 0.
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 char letter;
 int line;
-  for (line = 0; line <= 10; line++)
-    {
-      letter = 'a';
-      while (letter <= 'z')
-	{	  
-	  _putchar(letter);
-	  letter++
-	}
-      _putchar('\n')
-     }
+for (line = 0; line <= 10; line++)
+{
+letter = 'a';
+while (letter <= 'z')
+{  
+_putchar(letter);
+letter++;
 }
-2-print_alphabet_x10.c:12: WARNING: please, no spaces at the start of a line
-2-print_alphabet_x10.c:12: WARNING: suspect code indent for conditional statements (1, 6)
-2-print_alphabet_x10.c:13: WARNING: please, no spaces at the start of a line
-2-print_alphabet_x10.c:14: WARNING: please, no spaces at the start of a line
-2-print_alphabet_x10.c:15: WARNING: please, no spaces at the start of a line
-2-print_alphabet_x10.c:15: WARNING: suspect code indent for conditional statements (6, 10)
-2-print_alphabet_x10.c:16: ERROR: trailing whitespace
-2-print_alphabet_x10.c:20: WARNING: please, no spaces at the start of a line
-2-print_alphabet_x10.c:21: WARNING: please, no spaces at the start of a line
+_putchar('\n');
+}
+}
