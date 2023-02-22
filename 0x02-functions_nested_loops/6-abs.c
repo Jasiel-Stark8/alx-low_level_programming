19aeed278ca7da7f25daf94b139815a835d95694
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-22 02:45:16 -0800

    6-absolute zero value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - Entry point
+ * @n: n is an integer
+ * Description: prints the absolute value of an integer
+ * Return: int
+ */
+int _abs(int)
+{
+if (n < 0)
+{
+return (n)
+}
+else if (n > 0)
+{
+return (n)
+}
+else
+{
+return (n)
+}
