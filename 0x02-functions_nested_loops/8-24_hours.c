ac4ca32780a5c458ade2b05bce8a5a9678e8a41e
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-22 04:46:20 -0800

    Jack Bauer 24 Hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,17 +7,17 @@
  */
 void jack_bauer(void)
 {
-  int hour, minute;
-  for (hour = 0; hour < 24; hour++)
-    {
-      for (minute = 0; minute < 60; minute++)
-	{
-	  _putchar(hour / 10 + '0');
-	  _putchar(hour % 10 + '0');
-	  _putchar(':');
-	  _putchar(minute / 10 + '0');
-	  _putchar(minute % 10 + '0');
-	  _putchar('\n');
-	}
-    }
+int hour, minute;
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+_putchar(hour / 10 + '0');
+_putchar(hour % 10 + '0');
+_putchar(':');
+_putchar(minute / 10 + '0');
+_putchar(minute % 10 + '0');
+_putchar('\n');
+}
+}
 }
