88fdb4e88988fee33b8d1dfbb3709aae2c43077a
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-23 07:24:21 -0800

    number size comparison

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,25 +1,30 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: first int
- * @b: second int
- * @c: third int
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
  * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
 {
-	int largest = b;
+ int
+ largest;
 
-	if (a > b)
-	{
-		largest = a;
-	}
-	else if (c > b)
-	{
-		largest = c;
-	}
-
-	return (largest);
-}
+ if (a > b && a > c)
+ {
+ largest = a;
+ }
+ else if (a > b && c > a)
+ {
+ largest = c;
+ }
+ else if (b > c)
+ { largest = b;
+ }
+ else
+ {
+ largest = c;
+ }
