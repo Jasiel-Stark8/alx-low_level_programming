c0eaf07128f25427b7ebb8021ef536193a46a7f9
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-23 02:59:58 -0800

    Unit Test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -7,9 +6,7 @@
 int main(void)
 {
 int i;
-
-i = 98;
-positive_or_negative(i);
-
+i = 0;
+positive_or_negaive(i);
 return (0);
 }
