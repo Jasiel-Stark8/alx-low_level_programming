54e7649fa51a9bf2ba2d82d3f572ac215508a69c
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-23 05:38:30 -0800

    number comparison

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,15 +14,15 @@ int largest_number(int a, int b, int c)
 
     if (a > b && a > c)
     {
-      printf("%d is the largest number", a);
+        largest = a;
     }
     else if (b > a && b > c)
     {
-      printf("%d is the largest number", b);
+        largest = b;
     }
     else
     {
-      printf("%d is the largest number",c);
+        largest = c;
     }
 
     return (largest);
