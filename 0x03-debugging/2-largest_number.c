366e16e3a3fcbd1e2fe53e230b5a527e2920099a
Jasiel-Stark8 <JazielAir777@gmail.com>
2023-02-23 06:35:38 -0800

    number size comparison

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,18 +8,22 @@
  * Return: largest number
  */
 
-int largest_number(int a, int b, int c)
+int largest_number (int a, int b, int c)
 {
-	int largest = b;
-
-	if (a > b)
-	{
-	    largest = a;
-	}
-	else if (c > b)
-	{
-	    largest = c;
-	}
-
-	return (largest);
+int largest;
+if (a > b && a > c) 
+{
+largest = a;
+}
+else if (a > b && c> a)
+{ 
+largest = c;
+}
+else if (b > c)
+{ 
+largest = b;
+}
+else
+{
+largest = c;
 }
