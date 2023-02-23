5437108873b7efaac6a9858e14131bf0bdc48c77
Jason Kvïst <94616615+Jasiel-Stark8@users.noreply.github.com>
2023-02-23 00:19:07 +0000

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the sum of all multiples of 3 or 5 up to 102
+ * Return: Always (Success)
+ */
+int main(void)
+{
+    int sum = 0;
+    for (int i = 0; i < 1024; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    printf("%d\n", sum);
+    return 0;
+}
