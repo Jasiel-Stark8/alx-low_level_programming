79c5a24e08111bb5a8241ff022e52af93c44d207
Jason Kvïst <94616615+Jasiel-Stark8@users.noreply.github.com>
2023-02-23 00:04:16 +0000

    100-x-Tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,51 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_times_table - prints the n times table starting with 0
- * @n: the number of rows and columns in the table
+ * print_times_table - Prints a multiplication table up to param
+ * @n: The number to be treated
+ *
+ * Return: Number matrix
  */
 void print_times_table(int n)
 {
-if (n < 0 || n > 15)
-{
-return;
-}
-for (int row = 0; row <= n; row++)
-{
-for (int col = 0; col <= n; col++)
-{
-int value = row * col;
-int digits = 0;
-/** count the number of digits in the value*/
-if (value == 0)
-{
-digits = 1;
-}
-else
-{
-for (int temp = value; temp > 0; temp /= 10) {
-digits++;
-}
-}
-/** print the value with appropriate padding*/
-if (col > 0)
-{
-_putchar(' ');
-}
-for (int i = digits; i < 4; i++) {
-_putchar(' ');
-}
-if (value == 0)
-{
-_putchar('0');
-}
- else
-{
-for (int temp = value; temp > 0; temp /= 10)
-{
-_putchar(temp % 10 + '0');
-}
-}
-}
-_putchar('\n');
-}
+	int x, y, z;
+
+	if (n >= 0 && n <= 14)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
