2a5371b0d574cde1fdbde713dd8a47dad84544b0
Jasiel-Startk8 <JazielAir777@gmail.com>
2023-03-06 21:34:16 +0000

    setting memory buffer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,10 +13,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
   unsigned int i;
   
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < n; i++)
     {
       s[i] = b;
     }
 
-  return s
+  return (s);
 }
