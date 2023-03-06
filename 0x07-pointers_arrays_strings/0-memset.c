6837dfb3bb8192829a54ebffc202f5552348a734
Jasiel-Startk8 <JazielAir777@gmail.com>
2023-03-06 17:00:50 +0000

    setting memory buffer

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_memset: Fills memory with a constant byte
+ * @s: puts a char s
+ * @b: puts a char b
+ * @n: puts an integer n
+ *
+ * Returns: Pointer to memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+  char buffer[10];
+  int n;
+  
+  _memset(buffer, 's', sizeof(char) * 10);
+  for (n = 0; i < 10; i++)
+    {
+      _putchar(buffer[i]);
+    }
+  _putchar('\n');
+
+  return (buffer)
+}
