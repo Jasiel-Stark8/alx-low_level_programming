8296def2c1baa0ebc26e7c89dc622b7a5757413b
Jasiel-Startk8 <JazielAir777@gmail.com>
2023-03-07 17:31:29 +0000

    Filling Null byte to memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 #include <string.h>
 /**
  * *_memset: Fills memory with a constant byte
- * @s: puts a char s
- * @b: puts a char b
- * @n: puts an integer n
+ * @s: points to memory area
+ * @b: contstant byte
+ * @n: number of bytes to fill
  *
  * Returns: Pointer to memory area
  */
@@ -12,11 +12,9 @@
 char *_memset(char *s, char b, unsigned int n)
 {
   unsigned int i;
-  
   for (i = 0; i < n; i++)
     {
-      s[i] = b[i];
+      s[i] = b;
     }
-
   return (s);
 }
