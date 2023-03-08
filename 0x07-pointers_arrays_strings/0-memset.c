a01765f6cb685ea4e643909c1ae15d2979f0f0e4
Jasiel-Startk8 <JazielAir777@gmail.com>
2023-03-08 01:17:13 +0000

    Settle

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * *_memset - fills memory with a constant byte.
  * @s: pointer to put the constant
@@ -7,14 +6,14 @@
  * @n: max bytes to use
  * Return: s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i;
 
-	for (i = 0; n > 0; i++, n--)
+	for (i = 0; n > 0; i++)
 	{
 		s[i] = b;
+		n--;
 	}
 
 	return (s);
