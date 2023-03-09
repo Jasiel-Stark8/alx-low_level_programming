f04bd94e5ff43c65f88532303715f49be6e7d781
Jasiel-Startk8 <JazielAir777@gmail.com>
2023-03-09 10:04:17 +0000

    Printing recursive strings

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,16 @@
 #include "main.h"
-
 /**
  * _puts_recursion - Prints a string, followed by a new line.
  * @s: The string to be printed.
+ * Returns: Always true.
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+    if (*s)
+        {
+	   _putchar(*s);
+	   _puts_recursion(s + 1);
 	}
-
-	else
-		_putchar('\n');
+    else
+    _putchar('\n');
 }
