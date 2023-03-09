7b2fc16ca655f3b1ecd82fa78e5eb80dc4e53bb5
Jasiel-Startk8 <JazielAir777@gmail.com>
2023-03-09 12:51:43 +0000

    Puts recursive reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,9 +6,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-  if (*s)
-    {
-      _putchar(*s);
-      _print_rev_recursion(s + 1);
-    }
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
 }
