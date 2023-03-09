222a2913d9c769c7c7be8556e49cd048cad09a83
Jasiel-Startk8 <JazielAir777@gmail.com>
2023-03-09 16:13:10 +0000

    Calculate Power of 2 integers recursively

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recursion - Calculates the power of two integers
+ * @x: Base integer
+ * @y: Power base integer is raised to
+ *
+ * Return: True or false based on output
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+else
+return (x * _pow_recursion(x, y -1);
+}
