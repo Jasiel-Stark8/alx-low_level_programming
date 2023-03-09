4271a7187dced02a8470d4962691838e25af67d8
Jasiel-Startk8 <JazielAir777@gmail.com>
2023-03-09 11:23:55 +0000

    Puts recursive reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * 
- *
- *
+ *  _print_rev_recursion - prints recursive reverse string followed by a new line
+ * @s: string to be reversed
+ * Returns: Always True
  */
 void _print_rev_recursion(char *s)
 {
   if (*s)
     {
       _putchar(*s);
-      _putchar_rev_recursion(s - 1);
+      _print_rev_recursion(s - 1);
     }
   else
     {
