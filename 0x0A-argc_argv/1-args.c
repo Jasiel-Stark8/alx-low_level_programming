ae028370badb57137ccc322c63a6aff9162fdcdf
Jasiel-Startk8 <JazielAir777@gmail.com>
2023-03-13 09:39:11 +0000

    Print Number of Arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,6 +9,10 @@
  */
 int main(int argc, char *argv[])
 {
-  printf("Number of arguments passed is: %d\n", argc);
+  int i = 0;
+  for (; i < argc; i++)
+    {
+      printf("Name Of Program Is: %s\n", argv[0]);
+    }
   return (0);
 }
