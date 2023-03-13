62656673ef4f90a54609424d62eb83c184e871a8
Jason Kvïst <94616615+Jasiel-Stark8@users.noreply.github.com>
2023-03-13 10:45:31 +0000

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - Prints all arguments passed into main
  * @argc: Number of command line arguments
  * @argv: Array name
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
 	int i;
