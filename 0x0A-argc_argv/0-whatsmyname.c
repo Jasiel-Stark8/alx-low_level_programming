7af84c79e6fcad98429c1b6775bbf38209ba5ed8
Jason Kvïst <94616615+Jasiel-Stark8@users.noreply.github.com>
2023-03-13 09:26:19 +0000

    Print Program Name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
  * main - Entry point
- * @argc: Argument count from CL and name of program
- * @*argv[]: Array holding all arguments passed
+ * @argc: The number of arguments passed
+ * @argv: The array of arguments
  *
- * Returns: Always True 
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-  int i = 0;
-  for (; i < argc; i++)
-    {
-      printf("Name Of Program Is: %s\n", argv[0]);
-    }
-  return (0);
+    printf("%s\n", argv[0]);
+    return 0;
 }
