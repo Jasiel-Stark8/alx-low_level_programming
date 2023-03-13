fa3f0941f1e1def3e4c92f660eb5409ebb9f8794
Jason Kvïst <94616615+Jasiel-Stark8@users.noreply.github.com>
2023-03-13 10:56:59 +0000

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include "main.h"
-
 /**
  * main - Entry point
  * @argc: The number of arguments passed
@@ -14,15 +13,19 @@ int main(int argc, char *argv[])
 {
     int i, sum = 0;
 
-    if (argc == 1) {
+    if (argc == 1)
+    {
         printf("0\n");
         return 0;
     }
 
-    for (i = 1; i < argc; i++) {
+    for (i = 1; i < argc; i++)
+    {
         int j = 0;
-        while (argv[i][j]) {
-            if (!isdigit(argv[i][j])) {
+        while (argv[i][j])
+        {
+            if (!isdigit(argv[i][j]))
+            {
                 printf("Error\n");
                 return 1;
             }
