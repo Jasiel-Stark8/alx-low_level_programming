79a160fb8fcee83e96a7c15a75044d13898d3ccf
Jasiel-Startk8 <JazielAir777@gmail.com>
2023-03-13 11:38:19 +0000

    Prototype Files

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+
+	return (s);
+}
